7668b68a0a39df075f9ebcac8ec48d76e957ef7c
Luiza Pozzobon <luiza.pozzobon@gmail.com>
2022-06-17 12:00:39 -0300

    chore: update requirements and setup

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='src',
     packages=find_packages(),
     version='0.1.0',
-    description='A short description of the project.',
-    author='Your name (or your organization/company/team)',
+    description='On diffusion models fairness',
+    author='ia376',
     license='MIT',
 )
